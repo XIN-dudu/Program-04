fd598cc5c5db7d1fec0199e0b4b58557b2602007
liweiran-191 <15236634688@163.com>
2025-07-16 16:25:53 +0800

    Update serializers.py

--- Django/roadMonitor/serializers.py
+++ Django/roadMonitor/serializers.py
@@ -34,7 +34,7 @@ class RoadRecordSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = roadRecord
-        fields = ['disease_id', 'road_id', 'disease_type', 'severity']
+        fields = ['disease_id', 'road_id', 'description']
         extra_kwargs = {
             'road_id': {'required': True, 'min_value': 1},
             'disease_type': {'required': True},
@@ -42,28 +42,20 @@ class RoadRecordSerializer(serializers.ModelSerializer):
             'path': {'required': True, 'max_length': 255},
         }
 
-    def validate_length(self, value):
-        """验证裂缝长度必须为正数"""
-        if value <= 0:
-            raise serializers.ValidationError("裂缝长度必须大于0")
-        return value
-
-    def validate_area(self, value):
-        """验证病害面积必须为正数"""
-        if value <= 0:
-            raise serializers.ValidationError("病害面积必须大于0")
-        return value
-
     def to_representation(self, instance):
         """自定义序列化输出格式"""
         data = super().to_representation(instance)
         
-        # 替换数值为可读标签
-        data['disease_type'] = instance.get_disease_type_display()
-        data['severity'] = instance.get_severity_display()
-        url = os.path.join(settings.MEDIA_URL, 'road', 'results', instance.path)
-        # 添加额外信息
         data['detection_date'] = instance.detection_time.strftime("%Y-%m-%d")
-        data['url'] = 'http://localhost:8000' + url
-        
+
+        if isinstance(data['description'], list):
+            for item in data['description']:
+                # 将数字转换为文字描述
+                severity_mapping = {v[0]: v[1] for v in roadRecord.SEVERITY_CHOICES}
+                disease_type_mapping = {v[0]: v[1] for v in roadRecord.DISEASE_TYPE_CHOICES}
+                item['severity'] = severity_mapping.get(item.get('severity'), 'UNKNOWN')
+                item['disease_type'] = disease_type_mapping.get(item.get('disease_type'), 'UNKNOWN')
+                url = os.path.join(settings.MEDIA_URL, 'road', item['url'])
+                item['url'] = 'http://localhost:8000' + url
+                
         return data
