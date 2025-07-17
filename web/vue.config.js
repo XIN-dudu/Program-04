31a7115245d9f909aa77f5627f5b8036951bc135
ZhangJiaJiong <2640584193@qq.com>
2025-07-17 11:51:58 +0800

    fix:提交错误

--- web/vue.config.js
+++ web/vue.config.js
@@ -4,17 +4,17 @@ module.exports = {
   devServer: {
     proxy: {
       '/api': {
-        target: 'http://120.46.211.91:8000',
+        target: 'http://localhost:8000',
         changeOrigin: true,
         pathRewrite: { '^/api': '/api' }
       },
       '/road': {
-        target: 'http://120.46.211.91:8000',
+        target: 'http://localhost:8000',
         changeOrigin: true,
         pathRewrite: { '^/road': '/road' }
       },
       '/media': {
-        target: 'http://120.46.211.91:8000',
+        target: 'http://localhost:8000',
         changeOrigin: true,
         // 不需要pathRewrite，直接转发
       }
