5b6565f51eac77da346dcd190d3a315e0c97830e
ZhangJiaJiong <2640584193@qq.com>
2025-07-17 09:53:32 +0800

    fix:1

--- Django/Django/settings.py
+++ Django/Django/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-@l+)l^ure&c4_0$4(*@e4q&(il1a@*s)3dw4#3^p2orjo0k9+c"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS =120.46.2110.91 localhost', '127.0.0.1']
 
 
 # Application definition
